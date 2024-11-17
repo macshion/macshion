00770165b09c7c33d205f65423e9073554554556
Macshion <grt322@163.com>
2024-11-17 18:53:43 +0900

    UPdate

--- src/theme/DocItemFooter/DocItemFooter.js
+++ src/theme/DocItemFooter/DocItemFooter.js
@@ -1,13 +1,55 @@
 import React from 'react';
-import LastUpdated from '@theme/LastUpdated';
+import clsx from 'clsx';
+import { ThemeClassNames } from '@docusaurus/theme-common';
 
-export default function DocItemFooter ( props ) {
+function LastUpdatedAtDate ( { lastUpdatedAt } ) {
     return (
-        <footer>
-            {/* 自定义显示逻辑 */ }
-            <LastUpdated>
-                <span>自定义最終更新: 2018年10月14日</span>
-            </LastUpdated>
+        <>
+            { lastUpdatedAt && (
+                <div className={ clsx( ThemeClassNames.common.lastUpdated ) }>
+                    <span>最終更新: </span>
+                    <time dateTime={ new Date( lastUpdatedAt * 1000 ).toISOString() }>
+                        { new Date( lastUpdatedAt * 1000 ).toLocaleDateString( 'ja-JP', {
+                            year: 'numeric',
+                            month: 'long',
+                            day: 'numeric'
+                        } ) }
+                    </time>
+                </div>
+            ) }
+        </>
+    );
+}
+
+function LastUpdatedByUser ( { lastUpdatedBy } ) {
+    return lastUpdatedBy ? (
+        <div className="docLastUpdatedBy">
+            <span>更新者: { lastUpdatedBy }</span>
+        </div>
+    ) : null;
+}
+
+export default function DocItemFooter ( { content: DocContent } ) {
+    const { metadata } = DocContent;
+    const { lastUpdatedAt, lastUpdatedBy, frontMatter } = metadata;
+
+    const updateDate = frontMatter.last_update?.date
+        ? new Date( frontMatter.last_update.date ).getTime() / 1000
+        : lastUpdatedAt;
+
+    return (
+        <footer
+            className={ clsx( ThemeClassNames.docs.docFooter, 'docusaurus-mt-lg' ) }>
+            { ( updateDate || lastUpdatedBy ) && (
+                <div className="docLastUpdated">
+                    <LastUpdatedAtDate
+                        lastUpdatedAt={ updateDate }
+                    />
+                    <LastUpdatedByUser
+                        lastUpdatedBy={ lastUpdatedBy }
+                    />
+                </div>
+            ) }
         </footer>
     );
 }
